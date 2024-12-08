ffda399c878fe8bea0ed99abe6046245585570c4
Peter Große Gehling <peter.grosse-gehling@stud.tu-darmstadt.de>
2024-12-08 19:17:30 +0100

    Initialized Project

---
+++ src/test/java/com/arcticray/matchmaking_system/MatchmakingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcticray.matchmaking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
